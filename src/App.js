9e947115178d77410b96d778ea494f079fb0b8d0
Khalil <rashedki@hotmail.com>
2019-09-14 09:14:49 +0300

    creating input form with mirror effect
    This is my first practice of creating an input form that do mirroring for what I type...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+class App extends Component {
+  state = {
+    name : ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Form Part 1
+        <form>
+          <input type='text' onChange={this.handleChange}/>
+        </form>
+        <p>{this.state.name}</p>
+      </div>
+    )
+  }
+}
 export default App;
